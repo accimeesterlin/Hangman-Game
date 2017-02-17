635a7a24f945054f17ffef3ddf9b748321c4988a
Esterling Accime <accimeesterlin@yahoo.com>
2017-02-17 16:26:28 -0500

    Hangman game getting close

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,96 +28,127 @@ var hangman = {
 		this.grabId(el).textContent = val;
 	}
 
+
+
 };
 
+// Pseudo Code
 
 
-// Outputting values to the current word!
+
+
+
+// Store the computer typing result (word)
+	// No letter twice to the DOM
+	// Output every letter to the DOM every time we're typing
+
+
+// Guess remaining
+
+// Generate a new word
+	// Output this word to the current word in DOM with (---)
+	// Check if the letter from computer typing equal any letter from the word
+		// then grab the Dash function and output the word accordingly 
+	// Check if the new generated word is equal to the computer typing result
+	 	// Reset computer typing result
+	 	// Generate another new word everytime :
+			// Chances remaining is equal to 0
+			// Current word is equal to computer typing result (word)
+			// Output it the current word in DOM with (--)
+
 var word = hangman.randomWord();
 
+var arr = word.split("");
+console.log(word);
 
-hangman.grabId('newWord').addEventListener('click', function() {
-	 word = hangman.randomWord();
+
+
+// Replacing the word with the Dash
+piece = [];
+function replaceDash(){
+	return arr.map(function(element){
+					piece = [];
+					var result = element.replace(element, ' __ ');
+					piece.push(result);
+				});
+}
+
+replaceDash();
+//hangman.output("currentWord", piece);
+
+
+
+// Non Duplicated letters
+function nonDuplicated(){
+	typing.push(event.key);
+		norepeat = [];
+		norepeat = typing.filter(function(e, index, self) {
+		return index == typing.indexOf(e);
+	});
+
+	//console.log(norepeat);
+
 	
-});
-
- console.log(word);
+}
 
 
 
-// The array of letters from the random word
-var arr = word.split('');
+var typing = [];
+var norepeat = [];
+
+var wordMatch = [];
 
 
-
-// ---- replacing every single letter
-var piece = [];
-
-
-// Looping or mapping over the letters of the random word
-arr.map(function(element){
-	var result = element.replace(element, '_');
-	piece.push(result);
-});
-
-
-// Outputing it to the DOM
-
-var decreasing = hangman.guessRaiming;
-
-
-
-var newWord = hangman.randomWord();
-
-
-// Do this with the user's pressing a button
 document.onkeyup = function(event){
-
-	// Outputting the key letter to the DOM in the already guessed
-	var typing = document.createTextNode(event.key + ',');
-	hangman.grabId("Guessed").appendChild(typing);
-
-	arr.map(function(element, index){
-
-		if(element === event.key){
-			
-			piece[index] = element;
+nonDuplicated();
 
 
+hangman.output('Guessed', norepeat);	
+
+	arr.map(function(e, index) {
+		if(e === event.key){
+			wordMatch.push(e);
+			piece[index] = e;
+			hangman.output('currentWord', piece);
+
+
+			if(piece.join("") === word){
+				console.log("Yayyyy, it worked!");
+				norepeat = [];
+				hangman.output('Guessed', norepeat);
+				console.log(piece.join(""));
+
+				// word = hangman.randomWord();
+
+				// console.log(word);
+				// console.log(norepeat);
+				// replaceDash();
+				// console.log(piece);
+				// console.log(norepeat);
+				// hangman.output('win', hangman.win++);
+			}
 		}
 
-
-	}); // end of the mapping array
-	
-
-var currentWordString = piece.join("");
+		
+	});
 
 
-// Output to the current word to the DOM
-hangman.output('currentWord', currentWordString);
-
-//console.log(word);
-
-console.log(currentWordString);
-
-if(currentWordString === word){
-	// console.log(newWord);
-	hangman.output('win', "Your score is " + hangman.win++);
-
-}
-else{
-
-	hangman.output('guessRemaining', 'Your chances remaining is ' + decreasing--); 
-
-	console.log("They are not equal");
-}
-
-
-
+console.log(norepeat.join(''));
 
 };
 
-hangman.output('currentWord', piece);
+
+// Generate a new word
+	// Output this word to the current word in DOM with (---)
+	// Check if the letter from computer typing equal any letter from the word
+		// then grab the Dash function and output the letter accordingly 
+	// Check if the new generated word is equal to the word
+	 	// Reset computer typing result
+	 	// Generate another new word everytime :
+			// Chances remaining is equal to 0
+			// Current word is equal to computer typing result (word)
+			// Output it the current word in DOM with (--)
+
 
 
 
